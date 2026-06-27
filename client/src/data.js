c56087097b165961cf1f5811a2f44cbe0a5f8003
Ba8078509 <107764126+Ba8078509@users.noreply.github.com>
2026-06-27 19:44:55 +0300

    создал вывод каталога и курсов, добавил useState

---
+++ client/src/data.js
@@ -0,0 +1,37 @@
+const courses = [
+    {
+        id:1,
+        title: "Основы Монтажа",
+        lessons:[
+            {
+                id: 1 ,
+                title: "Введение в основы монтажа часть 1",
+                videoUrl: "https://youtube.com/watch?v=123"
+            },
+            {
+                id: 2,
+                title: "Введение в основы монтажа часть 2",
+                videoUrl: "https://youtube.com/watch?v=1234"
+            }
+        ]
+    },
+    {
+        id:2,
+        title: "Основы программирования",
+        lessons:[
+            {
+                id:3,
+                title: "Введение в основы программирования часть 1",
+                videoUrl: "https://youtube.com/watch?v=123"
+            },
+            {
+                id:4,
+                title: "Введение в основы программирования часть 2",
+                videoUrl: "https://youtube.com/watch?v=1234"
+            }
+        ]
+    }
+    
+]
+
+export default courses;
